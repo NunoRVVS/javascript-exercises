d1334a3908dbf2e14f4b17e86fbb49cf0e571bd2
Nunorvvs <nunor@Xubuntu>
2024-09-08 23:10:09 +0100

    Exercises 1 to 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...manyArgs) {
+    const newArray = [];
+    array.sort(function(a, b){return a - b});
+    array.forEach((item) => {
+        if(!manyArgs.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;   
 };
 
 // Do not edit below this line
